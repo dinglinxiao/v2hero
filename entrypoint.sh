584254747fc575f2dd60b217b57042ed8d8b0c0a
dinglinxiao <99388799+dinglinxiao@users.noreply.github.com>
2022-10-07 00:01:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,10 +38,12 @@ busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 mkdir -p ${DIR_CONFIG}
 #${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 cp ${DIR_TMP}/heroku.json ${DIR_CONFIG}/config.json
+ls ${DIR_CONFIG}
 
 # Install V2Ray
 install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
+ls ${DIR_CONFIG}
 
 # Run V2Ray
 ${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.json
